4afdbff8b666fa3103d90ca72abfa425f77658c1
Jean Crus <jean.carlos@pucpr.edu.br>
2019-02-14 15:50:30 -0200

    Aula 211 - Inserindo dados no banco 2

--- jdbc1/src/application/Program.java
+++ jdbc1/src/application/Program.java
@@ -20,7 +20,7 @@ public class Program {
 		
 		try {
 			conn = DB.getConnection();
-			
+			/*
 			st = conn.prepareStatement(
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
@@ -33,6 +33,11 @@ public class Program {
 			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
 			st.setDouble(4, 3000.0);
 			st.setInt(5, 4);
+			*/
+			st = conn.prepareStatement(
+					"INSERT INTO department (Name) values ('D1'),('D2')",
+					Statement.RETURN_GENERATED_KEYS);
+			
 			int rowsAffected = st.executeUpdate();
 			
 			if (rowsAffected > 0) {
@@ -47,9 +52,6 @@ public class Program {
 			}
 		}
 		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		catch(ParseException e) {
 			e.printStackTrace();
 		}
 		finally {
