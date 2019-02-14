db86dcb8a78f9257b07d5187ed3ce701c8909278
Jean Crus <jean.carlos@pucpr.edu.br>
2019-02-14 10:37:21 -0200

    Aula 210 - JDBC select

--- jdbc1/src/application/Program.java
+++ jdbc1/src/application/Program.java
@@ -1,15 +1,45 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Connection con = DB.getConnection();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
 		
-		DB.closeConnection();
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			
+			rs = st.executeQuery("select * from department");
+			
+			System.out.print("ID ");
+			System.out.print(" Name");
+			System.out.println();
+			
+			while (rs.next()) {
+				System.out.print(rs.getInt("Id") + " ");
+				System.out.print(" " + rs.getString("Name"));
+				System.out.println();
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
 	}
 
 }
