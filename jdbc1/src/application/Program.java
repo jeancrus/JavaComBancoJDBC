6b866d762fc5cddb70d22716aafff4d9905e4653
Jean Crus <jean.carlos@pucpr.edu.br>
2019-02-15 17:03:09 -0200

    Aula 213 - Deletar banco de dados com where

--- jdbc1/src/application/Program.java
+++ jdbc1/src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -15,19 +16,17 @@ public class Program {
 		try {
 			conn = DB.getConnection();
 			st = conn.prepareStatement(
-					"UPDATE seller "
-					+ "SET BaseSalary = BaseSalary + ?"
+					"DELETE FROM department "
 					+ "WHERE "
-					+ "(DepartmentID = ?)");
-			st.setDouble(1, 200.0);
-			st.setInt(2, 2);
+					+ "Id = ?");
+			st.setInt(1, 5);
 			
 			int rowsAffected = st.executeUpdate();
 			
 			System.out.println("Done! Rows affected: " + rowsAffected);	
 		} 
 		catch (SQLException e) {
-			e.printStackTrace();
+			throw new DbIntegrityException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(st);
